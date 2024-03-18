baf8701b579347656ddc818e23c27287b8d4e1e3
KaleabDereje <kaleabdereje388@gmail.com>
2024-03-18 22:58:34 +0300

    jsx rules

---
+++ memo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//stateless functional component
+//always return jsx
+
+
+function Greeting() {
+    return <h4>hello world</h4>;
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
