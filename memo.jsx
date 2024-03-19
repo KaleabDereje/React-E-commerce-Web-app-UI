8780bf97bbbe9c903d68df98ff72cbb91445ef66
KaleabDereje <kaleabdereje388@gmail.com>
2024-03-19 22:37:55 +0300

    jsx rules and nested components

--- memo.jsx
+++ memo.jsx
@@ -3,14 +3,45 @@ import ReactDOM from 'react-dom';
 
 //stateless functional component
 //always return jsx
-
+//react fragment
 
 function Greeting() {
-    return <h4>hello world</h4>;
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
 }
 
 //let root = ReactDOM.createoot(document.getElementById('root'));
 //root.render(<Greeting />);
-
 //ReactDOM.render(<Greeting />, document.getElementById('root'));
 
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
