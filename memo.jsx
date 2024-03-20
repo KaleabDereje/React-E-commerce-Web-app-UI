1f94b6054781b2d229540342abce9141ad94490a
KaleabDereje <kaleabdereje388@gmail.com>
2024-03-20 23:09:39 +0300

    book store page

--- memo.jsx
+++ memo.jsx
@@ -45,3 +45,319 @@ function Greeting() {
     }
 
 ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+//stateless functional component
+//always return jsx
+//react fragment
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
+
+
+function Greeting() {
+    return 
+    <React.fragment>
+        <Person />
+    <h4>This is my massage</h4>;
+    </React.fragment>
+}
+
+//let root = ReactDOM.createoot(document.getElementById('root'));
+//root.render(<Greeting />);
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+//nested components
+
+    const Person = () => {
+        return  <h2>kal kook</h2>;
+    }
+
+ReactDOM.render(<Greeting />, document.getElementById('root')); 
