1f0cc2ca20c64935eb109ce2997c20bb3c41f51e
KaleabDereje <kaleabdereje388@gmail.com>
2024-05-09 15:41:57 +0300

    change-1

--- src/App.jsx
+++ src/App.jsx
@@ -200,17 +200,24 @@ import React, {useState, useEffect} from 'react';
           </section>
           </>
         );
-      */
 
         // conditional rendering implementation
-        const url = 'https://api.github.com/users/QuincyLarson';
+        const url = 'https://api.github.com/users/QuincyLarsons';
         const [isLoading, setIsLoading] = useState(true);
         const [isError, setIsError] = useState(false);
         const [user, setUser] = useState('Default User');
 
         useEffect(() => {
           fetch(url)
-          .then((resp) => resp.json())
+          .then((resp) =>{
+            if(resp.status >= 200 && resp.status <= 299) {
+              return resp.json();
+            } else {
+              setIsLoading(false);
+              setIsError(true);
+              throw new Error(resp.statusText);
+            }
+          })
           .then((user) => {
             const {login} = user;
             setUser(login);
@@ -234,6 +241,9 @@ import React, {useState, useEffect} from 'react';
           return (
             <h1>{user}</h1>
           );
+        */
+       
+          //
 
 
   };
