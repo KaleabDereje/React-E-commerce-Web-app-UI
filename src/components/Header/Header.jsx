62904f0ed00c6d3cf90823ef31efcb62761d6153
KaleabDereje <kaleabdereje388@gmail.com>
2024-07-16 16:47:19 +0300

    home header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,15 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className='header'>
-
+        <div className='header'>  
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.</p>
+            <span><p>One Delicious meal ata time.</p></span>
+            <button>View Menu</button>
+        </div>
         </div>
     );
 }
 
-export default Header;
+export default Header; 
