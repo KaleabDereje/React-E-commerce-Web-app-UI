5efb028fceb6a2baa8f50d36c2591174d5e2ee4f
KaleabDereje <kaleabdereje388@gmail.com>
2024-07-22 23:26:58 +0300

    structuring the chunk

--- memo.jsx
+++ memo.jsx
@@ -972,4 +972,7 @@ today i didnt do anything
 today i spoke a friend about the road am gone take and nourishes myself that am gone be there on the station in the middle of the road
 for the time ahead put yourself and your goal first dont fool yourself and dont fall for promises
 today i did smt but not important and am on rage can feel it even on ma voice
+
+felt the wage but still trying to go straight
+telling ma self it's goin to as planned
 */
