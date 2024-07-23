7abd123e6bd2fbeb7a0dd2566cbd2e3f68914ab1
KaleabDereje <kaleabdereje388@gmail.com>
2024-07-23 13:41:06 +0300

    add item counter

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,11 +1,28 @@
 import { createContext } from "react";
 
 import {food_list} from "../assets/assets";
+import { useState } from "react";
 
 //create the context
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev,[itemId]:1}));
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    }
+
 
     const contextValue = {
         food_list
