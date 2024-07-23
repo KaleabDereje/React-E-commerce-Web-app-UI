422736a8839d3461f00358362770294ebadebe53
KaleabDereje <kaleabdereje388@gmail.com>
2024-07-23 12:41:39 +0300

    styling menu item lists

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,14 +1,24 @@
-import react from 'react';
+import react, { useState } from 'react';
 import './FoodItem.css';
 
 import {assets} from '../../assets/assets';
 
 const FoodItem = ({id, name, price, description, image}) => {
-   const newPrice = price + '0 Birr';
+
+    const [itemCount, setItemCount] = useState(0);
+
     return (
         <div className="food-item">
             <div className="food-item-img-container">
-                <img src={image} alt="" className="food-item-image" />
+                <img src={image} alt="" className="food-item-image" />                    
+                {!itemCount
+                    ? <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" className="add" />
+                    : <div className="food-item-counter">
+                        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+                        <p>{itemCount}</p>
+                        <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                    </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
@@ -16,7 +26,7 @@ const FoodItem = ({id, name, price, description, image}) => {
                     <img src={assets.rating_starts} alt="" />
                 </div>
                 <p className="food-item-desc">{description}</p>
-                <p className="food-item-price">{newPrice}</p>
+                <p className="food-item-price">{price} Birr</p>
             </div>
         </div>
     );
