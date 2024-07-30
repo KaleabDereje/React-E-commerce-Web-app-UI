f3797c453d37f6ce34c68410f800a969b109abcb
KaleabDereje <kaleabdereje388@gmail.com>
2024-07-30 18:21:19 +0300

    style footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,13 +9,14 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-content-left">
                     <img src={assets.logo} alt="" />
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
                     <img src={assets.twitter_icon} alt="" />
                     <img src={assets.linkedin_icon} alt="" />
                 </div>
+                </div>
+                
                 <div className="footer-content-center">
                     <h2>COMPANY</h2>
                     <ul>
