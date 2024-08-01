67cb3c2cd5f0d34bc98ba741c22a4b2ee8638b60
KaleabDereje <kaleabdereje388@gmail.com>
2024-08-01 13:41:37 +0300

    add App Download option and style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 import StoreContextProvider from '../../context/StoreContext';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
 
@@ -14,7 +15,8 @@ const Home = () => {
             <Header/>
             <StoreContextProvider>
             <ExploreMenu />
-            <FoodDisplay />    
+            <FoodDisplay />   
+            <AppDownload /> 
             </StoreContextProvider>
             
         </div>
